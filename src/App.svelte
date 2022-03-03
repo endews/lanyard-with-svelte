3c2d1b1bb1b5e04a0fefa2d74333a5991be3e149
Endew <88934866+endews@users.noreply.github.com>
2022-03-03 12:19:43 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Lanyard from './components/Lanyard.svelte'
+</script>
+
+<main>
+    <div>
+        <Lanyard id="577513401279053864" />
+    </div>
+</main>
---
+++ src/components/Lanyard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let id: number;
+
+    import { useLanyard } from "svelte-lanyard";
+
+    const lake = useLanyard(`${id}`)
+</script>
+
+<main>
+    {#if $lake}
+        <p>Hewwo I'm {$lake.discord_user.username}#{$lake.discord_user.discriminator} Nice to meet you :3</p>
+    {:else}
+        <p>Sorry I'm so shy :( </p>
+    {/if}
+</main>
